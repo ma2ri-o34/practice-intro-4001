93200a9bb4c3abb84ac359a8de286013a1b55c40
ma2ri0 <84706241+ma2ri-o34@users.noreply.github.com>
2025-02-10 00:26:04 +0900

    ルートの追加

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const { Hono } = require("hono");
-const { serve } = require("@hono/node-server");
-const { logger } = require("hono/logger");
-const { html } = require("hono/html");
+const { Hono } = require('hono');
+const { serve } = require('@hono/node-server');
+const { logger } = require('hono/logger');
+const { html } = require('hono/html');
 
 const app = new Hono();
 app.use(logger());
 
-app.get("/", (c) => {
-  const name = c.req.query("name") ?? "ゲスト";
+app.get('/', (c) => {
+  const name = c.req.query('name') ?? 'ゲスト';
   return c.html(html`
     <!doctype html>
     <html>
@@ -17,6 +17,18 @@ app.get("/", (c) => {
       <body>
         <h1>こんにちは！ ${name} さん</h1>
         <p>これは Hono のサンプルアプリケーションです。</p>
+      </body>
+    </html>
+  `);
+});
+
+app.get('/secret', (c) => {
+  return c.html(html`
+    <!doctype html>
+    <html>
+      <body>
+        <h1>秘密のページ</h1>
+        <p>見つけてしまいましたね...</p>
       </body>
     </html>
   `);
